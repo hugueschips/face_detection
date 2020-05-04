37d250f74d6852f98e30533e6d2b9e83a1951b76
Daniel Durrenberger <dd.durrenberger@gmail.com>
2020-05-04 21:51:48 +0200

    scale depends on img.shape

---
+++ bubble.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+
+class Bubble:
+    def __init__(self, img, pt1, pt2, text, bg_color, font_face, scale, thickness, margin):
+        if not text: return None
+        self.img = img
+        self.bg_color = bg_color
+        self.font_face = font_face
+        self.scale = scale
+        self.thickness = thickness
+        self.margin = margin
+        self.pt1 = pt1
+        self.pt2 = pt2
+        self.text = text
+        self.p1 = self.get_p1()
+        self.p2 = self.get_p2()
+        self.triangle = self.get_triangle()
+        self.x_text = self.p1[0] + self.margin
+        self.y_text = self.p2[1] + self.dy + self.margin
+
+    def get_p1(self):
+        return (self.pt2[0] + 4 * (self.pt2[0] - self.pt1[0]), self.pt2[1])
+
+    def get_p2(self):
+        n_lines = len(self.text)
+        longuest_line = max(self.text, key=len)
+        txt_size = cv2.getTextSize(longuest_line, self.font_face, self.scale, self.thickness)
+        self.dy = txt_size[0][1] + 3
+        x1 = self.p1[0]
+        y1 = self.p1[1]
+        x2 = x1 + txt_size[0][0] + 2*self.margin
+        y2 = y1 - n_lines * self.dy - 2*self.margin
+        return (x2, y2)
+
+    def get_triangle(self):
+        def get_coord_on_line(t, x, y):
+            return int(t * y + (1 - t) * x)
+        x1 = self.p1[0]
+        y1 = self.p1[1]
+        x2 = self.p2[0]
+        y2 = self.p2[1]
+        p3 = [get_coord_on_line(2. / 7., x1, x2), y1]
+        p4 = [x1, self.pt1[1]]
+        p5 = [get_coord_on_line(3. / 7., x1, x2), y1]
+        triangle = np.array([p3, p4, p5], np.int32)
+        triangle = triangle.reshape((-1, 1, 2))
+        return triangle
+
+    def get_new_line_y(self):
+        self.y_text += self.dy
+
+    def uncrop(self):
+        h, l, d = self.img.shape
+        p4 = self.triangle[1,0,:]
+        p4_not_in_img = (p4[0] > l-4*self.margin) or (p4[1] > h-4*self.margin)
+        if p4_not_in_img:
+            self.text = None
+            return None
+        upper_right_corner_not_in_img = (self.p2[0]>l) or (self.p2[1]>h)
+        for i in range(25):
+            if not upper_right_corner_not_in_img:
+                break
+            self.scale *= .9
+            self.margin = int(self.margin * .9)
+            self.p2 = self.get_p2()
+            self.triangle = self.get_triangle()
+            self.x_text = self.p1[0] + self.margin
+            self.y_text = self.p2[1] + self.dy + self.margin
+            upper_right_corner_not_in_img = (self.p2[0]>l) or (self.p2[1]>h)
+        if upper_right_corner_not_in_img:
+            self.text = None
+            return None
+
+    def draw(self, img):
+        if self.text:
+            cv2.rectangle(img, self.p1, self.p2, (255, 255, 255), cv2.FILLED)
+            cv2.rectangle(img, self.p1, self.p2, (0, 0, 0), self.thickness)
+            cv2.fillConvexPoly(img, self.triangle, (0, 0, 0), self.thickness)
+            for line in self.text:
+                color = (0,0,0) #text_color(line)
+                cv2.putText(img, line, (self.x_text, self.y_text), self.font_face, self.scale, color, 1, cv2.LINE_AA)
+                self.get_new_line_y()
+
+    def check_text_is_inside(self):
+        assert self.x_text > self.p1[0]
+        assert self.y_text > self.p2[1]
+        if self.y_text <= self.p2[1]:
+            print(f'p2 y is {{self.p2[1]}} and text y is {{self.y_text}}')
--- visage_camera.py
+++ visage_camera.py
@@ -1,7 +1,7 @@
-import numpy as np
 import cv2
 import json
 from time import sleep
+from bubble import Bubble
 
 debug = True
 detect_faces = False
@@ -30,78 +30,13 @@ def get_text(text, text2):
     out = []
     for item in text:
         if item:
-            out.append(item.title())
+            out.append(item.capitalize())
     if out:
         out.insert(0, 'FACE')
     if text2:
         out.append('VOICE')
-        out.append(text2.title())
+        out.append(text2.capitalize())
     return(out)
-
-def text_color(text):
-    return (0, 255, 0)
-
-class Bubble:
-    def __init__(self, pt1, pt2, text, bg_color, font_face, scale, thickness, margin):
-        if not text: return None
-        self.bg_color = bg_color
-        self.font_face = font_face
-        self.scale = scale
-        self.thickness = thickness
-        self.margin = margin
-        self.pt1 = pt1
-        self.pt2 = pt2
-        self.text = text
-        self.p1 = self.get_p1()
-        self.p2 = self.get_p2(text)
-        self.triangle = self.get_triangle()
-        self.x_text = self.p1[0]
-        self.y_text = self.p2[1] + self.dy
-
-    def get_p1(self):
-        return (self.pt2[0] + 4 * (self.pt2[0] - self.pt1[0]), self.pt2[1])
-
-    def get_p2(self, text):
-        n_lines = len(text)
-        longuest_line = max(text, key=len)
-        txt_size = cv2.getTextSize(longuest_line, self.font_face, self.scale, self.thickness)
-        self.dy = txt_size[0][1] + 3
-        x1 = self.p1[0]
-        y1 = self.p1[1]
-        x2 = x1 + txt_size[0][0] + self.margin
-        y2 = y1 - n_lines * self.dy - self.margin
-        return (x2, y2)
-
-    def get_triangle(self):
-        def get_coord_on_line(t, x, y):
-            return int(t * y + (1 - t) * x)
-        x1 = self.p1[0]
-        y1 = self.p1[1]
-        x2 = self.p2[0]
-        y2 = self.p2[1]
-        p3 = [get_coord_on_line(2. / 7., x1, x2), y1]
-        p4 = [x1, self.pt1[1]]
-        p5 = [get_coord_on_line(3. / 7., x1, x2), y1]
-        triangle = np.array([p3, p4, p5], np.int32)
-        triangle = triangle.reshape((-1, 1, 2))
-        return triangle
-
-    def get_new_line_y(self):
-        self.y_text += self.dy
-
-def draw_label(img, text, pt1, pt2):
-    bg_color = (0, 255, 0)
-    font_face = cv2.FONT_HERSHEY_SIMPLEX
-    scale = .8
-    thickness = 2
-    margin = 5
-    b = Bubble(pt1, pt2, text, bg_color, font_face, scale, thickness, margin)
-    cv2.rectangle(img, b.p1, b.p2, b.bg_color, b.thickness)
-    cv2.fillConvexPoly(img, b.triangle, b.bg_color)
-    for line in b.text:
-        color = text_color(text)
-        cv2.putText(img, line, (b.x_text, b.y_text), b.font_face, b.scale, color, 1, cv2.LINE_AA)
-        b.get_new_line_y()
 
 init_index_face = {key : 0 for key in emo_dic.keys()}
 while True:
@@ -114,7 +49,6 @@ while True:
         idx_face[key] = get_index_list(ts, fps, value, init_index_face[key])
         if idx_face[key]:
             init_index_face[key] = idx_face[key]
-
 
     ## Get face with biggest mouth
     biggest_mouth_key = next(iter(pos_dic))
@@ -134,7 +68,12 @@ while True:
         if idx_voice < len(aud_dic):
             voice_dic[biggest_mouth_key] = aud_dic[idx_voice]['emotion']
 
-    ## Draw labels
+    ## Draw bubbles
+    bg_color = (0, 255, 0)
+    font_face = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 2 * frame.shape[1]/1280.
+    thickness = 4
+    margin = 20
     for key, idx in idx_face.items():
         if idx:
             text = [emo_dic[key][idx]['emotion'], emo_dic[key][idx]['meaningful_expression']]
@@ -142,7 +81,10 @@ while True:
             x2, y2 = int(pos_dic[key][idx]['x2']), int(pos_dic[key][idx]['y2'])
             text = get_text(text, voice_dic[key])
             if text:
-                draw_label(frame, text, (x1, y1), (x2, y2))
+                bubble = Bubble(frame, (x1, y1), (x2, y2), text, bg_color, font_face, scale, thickness, margin)
+                bubble.uncrop()
+                bubble.draw(frame)
+
     ## Detect faces
     if detect_faces:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -150,7 +92,7 @@ while True:
         for x, y, w, h in face:
            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    ## Slow down and print timestamps
+    ## Debug mode
     if debug:
         sleep(0.001)
         cv2.putText(frame, '{:.5}'.format(ts), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
@@ -158,6 +100,7 @@ while True:
     ## Wait for "q" key
     if cv2.waitKey(1) == ord('q'):
         break
+
     cv2.imshow('video', frame)
 cap.release()
 cv2.destroyAllWindows()
